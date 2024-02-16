db7155a13d88f14e32076af5ac9e78b4dd1e6994
Johann <johann@lithium>
2024-02-16 08:16:41 -0500

    Genesis

---
+++ src/test/java/co/harborbytes/wortedeck/WorteDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package co.harborbytes.wortedeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorteDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
